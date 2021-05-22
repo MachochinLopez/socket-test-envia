8a9c5dd2e328402c14299fea20e6aebc57d338f1
Oscar López <machochinnio@hotmail.com>
2021-05-22 18:52:14 -0500

    Configuraciones de Vercel y rutas de prueba.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
     return view('realtime-counter');
 });
+
+Route::get('/guide-notification', [NotificationController::class, 'guideNotification']);
